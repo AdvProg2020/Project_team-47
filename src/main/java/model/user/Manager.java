c5ffc11000b31cd461451d023648f3103c8a166e
alipour79 <56475612+alipour79@users.noreply.github.com>
2020-05-02 17:43:21 +0430

    Complete Manager class

--- src/main/java/model/user/Manager.java
+++ src/main/java/model/user/Manager.java
@@ -1,11 +1,28 @@
 package model.user;
 
+import com.google.gson.Gson;
+import model.send.receive.UserInfo;
+
+import java.util.HashMap;
+
 public class Manager extends User {
     public Manager() {
+        super();
+    }
+
+    public Manager(HashMap<String, String> userInfo) {
+        super(userInfo);
+        User.managerAdded();
     }
 
     @Override
     public String userInfoForSending() {
-        return null;
+        UserInfo user = new UserInfo();
+        user.setEmail(this.getEmail());
+        user.setFirstName(this.getFirstName());
+        user.setLastName(this.getLastName());
+        user.setPhoneNumber(this.getPhoneNumber());
+        user.setUsername(this.getUsername());
+        return (new Gson()).toJson(user);
     }
 }
