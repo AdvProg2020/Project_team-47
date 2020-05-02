15b9f468eb5dcb9e7b3a723bdecd20cab8359c43
alipour79 <56475612+alipour79@users.noreply.github.com>
2020-05-02 17:52:21 +0430

    Complete Sort class

--- src/main/java/model/others/Sort.java
+++ src/main/java/model/others/Sort.java
@@ -3,16 +3,316 @@ package model.others;
 import model.category.Category;
 import model.discount.DiscountCode;
 import model.discount.Off;
+import model.log.Log;
+import model.others.request.Request;
 import model.user.User;
 
 import java.util.ArrayList;
 
 public class Sort {
-    public static ArrayList<Category> sortCategory(String field,String direction,ArrayList<Category> categories){return null;}
-    public static ArrayList<Product> sortProduct(String field,String direction,ArrayList<Product> products){return null;}
-    public static ArrayList<DiscountCode> sortDiscountCode(String field,String direction,ArrayList<DiscountCode> discountCodes){return null;}
-    public static ArrayList<Off> sortOffs(String field,String direction,ArrayList<Off> offs){return null;}
-    public static ArrayList<User> sortUsers(String field,String direction,ArrayList<User> users){return null;}
-    public static ArrayList<Request> sortRequest(String field,String direction,ArrayList<Request> requests){return null;}
+    public static ArrayList<Category> sortCategory(String field, String direction, ArrayList<Category> categories) {
+        ArrayList<Category> categoriesClone = (ArrayList<Category>) categories.clone();
+        if (field == null || direction == null) {
+            return categoriesClone;
+        }
+        if (field.equals("name")) {
+            categoriesClone.sort((o1, o2) -> {
+                if (direction.startsWith("a")) {
+                    return o1.getName().compareTo(o2.getName());
+                } else if (direction.startsWith("d")) {
+                    return o2.getName().compareTo(o1.getName());
+                }
+                return 1;
+            });
+        }
+        return categoriesClone;
+    }
+
+    public static ArrayList<Product> sortProduct(String field, String direction, ArrayList<Product> products) {
+        ArrayList<Product> productsClone = (ArrayList<Product>) products.clone();
+        if (field == null || direction == null) {
+            return productsClone;
+        }
+        switch (field) {
+            case "name":
+                sortProductByName(productsClone, direction);
+                break;
+            case "score":
+                sortProductByScore(productsClone, direction);
+                break;
+            case "seen-time":
+                sortProductBySeenTime(productsClone, direction);
+                break;
+            case "date":
+                sortProductByDate(productsClone, direction);
+                break;
+        }
+        return productsClone;
+    }
+
+    private static void sortProductByName(ArrayList<Product> products, String direction) {
+        products.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return o1.getName().compareTo(o2.getName());
+            } else if (direction.startsWith("d")) {
+                return o2.getName().compareTo(o1.getName());
+            }
+            return 1;
+        });
+    }
+
+    private static void sortProductByScore(ArrayList<Product> products, String direction) {
+        products.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return Double.compare(o1.getScoreAverage(), o2.getScoreAverage());
+            } else if (direction.startsWith("d")) {
+                return Double.compare(o2.getScoreAverage(), o1.getScoreAverage());
+            }
+            return 1;
+        });
+    }
+
+    private static void sortProductBySeenTime(ArrayList<Product> products, String direction) {
+        products.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return Integer.compare(o1.getSeenTime(), o2.getSeenTime());
+            } else if (direction.startsWith("d")) {
+                return Integer.compare(o2.getSeenTime(), o1.getSeenTime());
+            }
+            return 1;
+        });
+    }
+
+    private static void sortProductByDate(ArrayList<Product> products, String direction) {
+        products.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return Date.compare(o1.getCreatingDate(), o2.getCreatingDate());
+            } else if (direction.startsWith("d")) {
+                return Date.compare(o2.getCreatingDate(), o1.getCreatingDate());
+            }
+            return 1;
+        });
+    }
+
+    public static ArrayList<DiscountCode> sortDiscountCode(String field, String direction, ArrayList<DiscountCode> discountCodes) {
+        ArrayList<DiscountCode> discountCodesClone = (ArrayList<DiscountCode>) discountCodes.clone();
+        if (field == null || direction == null) {
+            return discountCodesClone;
+        }
+        switch (field) {
+            case "start-time":
+                sortDiscountWithStartTime(discountCodesClone, direction);
+                break;
+            case "finish-time":
+                sortDiscountWithFinishTime(discountCodesClone, direction);
+                break;
+            case "percent":
+                sortDiscountCodeByPercent(discountCodesClone, direction);
+                break;
+        }
+        return discountCodesClone;
+    }
+
+    private static void sortDiscountCodeByPercent(ArrayList<DiscountCode> discountCodes, String direction) {
+        discountCodes.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return Integer.compare(o1.getDiscountPercent(), o2.getDiscountPercent());
+
+            } else if (direction.startsWith("d")) {
+                return Integer.compare(o2.getDiscountPercent(), o1.getDiscountPercent());
+            }
+            return 1;
+        });
+    }
+
+    private static void sortDiscountWithStartTime(ArrayList<DiscountCode> discountCodes, String direction) {
+        discountCodes.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return Date.compare(o1.getDiscountStartTime(), o2.getDiscountStartTime());
+
+            } else if (direction.startsWith("d")) {
+                return Date.compare(o2.getDiscountStartTime(), o1.getDiscountStartTime());
+            }
+            return 1;
+        });
+    }
+
+    private static void sortDiscountWithFinishTime(ArrayList<DiscountCode> discountCodes, String direction) {
+        discountCodes.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return Date.compare(o1.getDiscountFinishTime(), o2.getDiscountFinishTime());
+
+            } else if (direction.startsWith("d")) {
+                return Date.compare(o2.getDiscountFinishTime(), o1.getDiscountFinishTime());
+            }
+            return 1;
+        });
+    }
+
+    public static ArrayList<Off> sortOffs(String field, String direction, ArrayList<Off> offs) {
+        ArrayList<Off> offsClone = (ArrayList<Off>) offs.clone();
+        if (field == null || direction == null) {
+            return offsClone;
+        }
+        switch (field) {
+            case "start-time":
+                sortOffWithStartTime(offsClone, direction);
+                break;
+            case "finish-time":
+                sortOffWithFinishTime(offsClone, direction);
+                break;
+            case "percent":
+                sortOffByPercent(offsClone, direction);
+                break;
+        }
+        return offsClone;
+    }
+
+    private static void sortOffByPercent(ArrayList<Off> offs, String direction) {
+        offs.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return Integer.compare(o1.getDiscountPercent(), o2.getDiscountPercent());
+
+            } else if (direction.startsWith("d")) {
+                return Integer.compare(o2.getDiscountPercent(), o1.getDiscountPercent());
+            }
+            return 1;
+        });
+    }
+
+    private static void sortOffWithStartTime(ArrayList<Off> offs, String direction) {
+        offs.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return Date.compare(o1.getDiscountStartTime(), o2.getDiscountStartTime());
+
+            } else if (direction.startsWith("d")) {
+                return Date.compare(o2.getDiscountStartTime(), o1.getDiscountStartTime());
+            }
+            return 1;
+        });
+    }
+
+    private static void sortOffWithFinishTime(ArrayList<Off> offs, String direction) {
+        offs.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return Date.compare(o1.getDiscountFinishTime(), o2.getDiscountFinishTime());
+
+            } else if (direction.startsWith("d")) {
+                return Date.compare(o2.getDiscountFinishTime(), o1.getDiscountFinishTime());
+            }
+            return 1;
+        });
+    }
+
+    public static ArrayList<User> sortUsers(String field, String direction, ArrayList<User> users) {
+        ArrayList<User> usersClone = (ArrayList<User>) users.clone();
+        if (field == null || direction == null) {
+            return usersClone;
+        }
+
+        switch (field) {
+            case "first-name":
+                sortUserByFirstName(usersClone, direction);
+                break;
+            case "last-name":
+                sortUserByLastName(usersClone, direction);
+                break;
+            case "username":
+                sortUserByUserName(usersClone, direction);
+                break;
+        }
+        return usersClone;
+    }
+
+    private static void sortUserByFirstName(ArrayList<User> users, String direction) {
+        users.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return o1.getFirstName().compareTo(o2.getFirstName());
+            } else if (direction.startsWith("d")) {
+                return o2.getFirstName().compareTo(o1.getFirstName());
+            }
+            return 1;
+        });
+    }
+
+    private static void sortUserByLastName(ArrayList<User> users, String direction) {
+        users.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return o1.getLastName().compareTo(o2.getLastName());
+            } else if (direction.startsWith("d")) {
+                return o2.getLastName().compareTo(o1.getLastName());
+            }
+            return 1;
+        });
+    }
+
+    private static void sortUserByUserName(ArrayList<User> users, String direction) {
+        users.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return o1.getUsername().compareTo(o2.getUsername());
+            } else if (direction.startsWith("d")) {
+                return o2.getUsername().compareTo(o1.getUsername());
+            }
+            return 1;
+        });
+    }
+
+
+    public static ArrayList<Request> sortRequest(String field, String direction, ArrayList<Request> requests) {
+        ArrayList<Request> requestsClone = (ArrayList<Request>) requests.clone();
+        if (direction == null || field == null) {
+            return requestsClone;
+        }
+        switch (field) {
+            case "apply-date":
+                sortRequestByApplyDate(requestsClone, direction);
+                break;
+            case "sender-username":
+                sortRequestBySenderUsername(requestsClone, direction);
+                break;
+        }
+        return requestsClone;
+    }
+
+    private static void sortRequestByApplyDate(ArrayList<Request> requests, String direction) {
+        requests.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return Date.compare(o1.getApplyDate(), o2.getApplyDate());
+            } else if (direction.startsWith("d")) {
+                return Date.compare(o2.getApplyDate(), o1.getApplyDate());
+            }
+            return 1;
+        });
+    }
+
+    private static void sortRequestBySenderUsername(ArrayList<Request> requests, String direction) {
+        requests.sort((o1, o2) -> {
+            if (direction.startsWith("a")) {
+                return o1.getRequestSender().getUsername().compareTo(o2.getRequestSender().getUsername());
+            } else if (direction.startsWith("d")) {
+                return o2.getRequestSender().getUsername().compareTo(o1.getRequestSender().getUsername());
+            }
+            return 1;
+        });
+    }
+
+    public static ArrayList<Log> sortLogs(String field, String direction, ArrayList<Log> logs) {
+        ArrayList<Log> logsClone = (ArrayList<Log>) logs.clone();
+        if (direction == null || field == null) {
+            return logsClone;
+        }
+        switch (field) {
+            case "money":
+                logsClone.sort((o1, o2) -> {
+                    if (direction.startsWith("a")) {
+                        return Double.compare(o1.getMoney(), o2.getMoney());
+                    } else if (direction.startsWith("d")) {
+                        return Double.compare(o1.getMoney(), o2.getMoney());
+                    }
+                    return 1;
+                });
+        }
+        return logsClone;
+    }
 
 }
