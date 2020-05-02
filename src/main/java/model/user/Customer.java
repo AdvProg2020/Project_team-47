b2f479833e87c9192244b6473dd4158af149dfb2
alipour79 <56475612+alipour79@users.noreply.github.com>
2020-05-02 17:45:30 +0430

    Complete Customer class

--- src/main/java/model/user/Customer.java
+++ src/main/java/model/user/Customer.java
@@ -1,11 +1,15 @@
 package model.user;
 
-import model.log.Log;
+import com.google.gson.Gson;
 import model.discount.DiscountCode;
+import model.log.Log;
 import model.others.Product;
+import model.others.Score;
 import model.others.ShoppingCart;
+import model.send.receive.UserInfo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Customer extends User {
     private ArrayList<Log> buyLogs;
@@ -14,28 +18,95 @@ public class Customer extends User {
     private double money;
 
     public Customer() {
+        super();
     }
 
-    public boolean userHasDiscountCode(String code){return true;}
-    public boolean canUserBuy(double cartPrice){return true;}
+    public Customer(HashMap<String, String> userInfo) {
+        super(userInfo);
+    }
 
-    private void addBuyLog(Log buyLog){}
-    public void buy(Log buyLog){}
+    public boolean userHasDiscountCode(String code) {
+        for (DiscountCode discountCode : this.discountCodes) {
+            if (code.equals(discountCode.getDiscountCode())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    public boolean doesUserBoughtProduct(Product product){return true;}
-    public String getAllDiscountCodeInfo(){return null;}
+    public boolean canUserBuy(double cartPrice) {
+        return !(this.money < cartPrice);
+    }
+
+    private void addBuyLog(Log buyLog) {
+        buyLogs.add(buyLog);
+    }
+
+    public void buy(Log buyLog) {
+    }
+
+    public boolean doesUserBoughtProduct(Product product) {
+        for (Log buyLog : buyLogs) {
+            if (buyLog.isThereProductInLog(product)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getAllDiscountCodeInfo() {
+        ArrayList<String> discountCodesInfo = new ArrayList<>();
+        for (DiscountCode discountCode : this.discountCodes) {
+            discountCodesInfo.add(discountCode.discountInfoForSending());
+        }
+        Gson discountCodesGson = new Gson();
+        return discountCodesGson.toJson(discountCodesInfo);
+    }
 
 
-    public String getAllOrdersInfo(){return null;}
+    public String getAllOrdersInfo() {
+        ArrayList<String> ordersInfo = new ArrayList<>();
+        for (Log buyLog : buyLogs) {
+            ordersInfo.add(buyLog.getLogInfoForSending());
+        }
+        Gson ordersInfoGson = new Gson();
+        return ordersInfoGson.toJson(ordersInfo);
+    }
 
 
+    public boolean doesCustomerOrdered(String productId) {
+        Product product = Product.getProductWithId(productId);
+        return doesUserBoughtProduct(product);
+    }
 
-    public boolean doesCustomerOrdered(String productId){return true;}
 
-    public void rate(int score,String productId){}
+    public void rate(int score, Product product) {
+        Score newScore = new Score();
+        newScore.setProduct(product);
+        newScore.setScore(score);
+        newScore.setWhoSubmitScore(this);
+        product.addScore(newScore);
+    }
 
     @Override
     public String userInfoForSending() {
-        return null;
+        UserInfo user = new UserInfo();
+        user.setEmail(this.getEmail());
+        user.setFirstName(this.getFirstName());
+        user.setLastName(this.getLastName());
+        user.setPhoneNumber(this.getPhoneNumber());
+        user.setUsername(this.getUsername());
+        user.setMoney(this.money);
+        return (new Gson()).toJson(user);
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void addDiscountCode(DiscountCode discountCode) {
+        if (!this.discountCodes.contains(discountCode)) {
+            this.discountCodes.add(discountCode);
+        }
     }
 }
