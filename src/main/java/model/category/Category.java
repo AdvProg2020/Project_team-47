26d3b80860806e65790a27fdb8828c89b5ee4743
mahdi hadi <58135026+mahdihadiam@users.noreply.github.com>
2020-05-03 12:01:09 +0430

    Overall look at category

--- src/main/java/model/category/Category.java
+++ src/main/java/model/category/Category.java
@@ -1,65 +1,108 @@
 package model.category;
 
 import model.others.Product;
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
-abstract public class Category {
-    private static ArrayList<Category> allMainCategories;
-    private static ArrayList<Category> allSubCategories;
-    protected String name;
-    protected ArrayList<String> specialProperties;
-    protected ArrayList<Product> allProducts;
-    protected ArrayList<Category> subCategories;
+public abstract class Category {
+    private static ArrayList<SubCategory> subCategories;
+    private String name;
+    private HashMap<String, String> properties;
+    private ArrayList<Product> products;
 
-
-    public Category() {
+    public Category(String name) {
+        this.name = name;
     }
 
-    abstract public String categoryInfoForSending();
-
-
-
-    public static String getAllCategoriesInfo(String sortField,String sortDirection){return null;}
-    public static void removeCategory(String categoryName){}
-    public static Category getCategoryByName(String name){return null;}
-
-
-
-    public static boolean isThereMainCategory(String categoryName){return true;}
-    public static boolean isThereSubCategory(String categoryName){return true;}
-
-
-
-
-
-    public boolean isThereProduct(Product product) {return true;}
-    public void addProduct(Product product){}
-    public void addSpecialProperties(String properties){}
-    public void removeSpecialProperties(String properties){}
-
-
-    public String getName() {
-        return name;
+    public static boolean doesSubCategoryExist(String name) {
+        Iterator<SubCategory> iterator = Category.subCategories.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getName().equalsIgnoreCase(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public ArrayList<String> getSpecialProperties() {
-        return specialProperties;
+    protected static void addSubCategory(SubCategory subCategory) {
+        Category.subCategories.add(subCategory);
     }
 
-    public ArrayList<Product> getAllProducts() {
-        return allProducts;
+    public static void removeSubCategory(String name) {
+        Iterator<SubCategory> iterator = Category.subCategories.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getName().equalsIgnoreCase(name)) {
+                iterator.remove();
+                break;
+            }
+        }
+    }
+
+    public static SubCategory getSubCategory(String name) {
+        Iterator<SubCategory> iterator = Category.subCategories.iterator();
+        while (iterator.hasNext()) {
+            SubCategory subCategory = iterator.next();
+            if (subCategory.getName().equalsIgnoreCase(name)) {
+                return SubCategory;
+            }
+        }
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setSpecialProperties(ArrayList<String> specialProperties) {
-        this.specialProperties = specialProperties;
+    public String getName() {
+        return name;
     }
 
-    public void setAllProducts(ArrayList<Product> allProducts) {
-        this.allProducts = allProducts;
+    /**
+     * each category holds properties in format of title-info
+     * 
+     * @param title the title of information.
+     * @param info  this String-kind details are just provided for displaying and
+     *              uses of manager.
+     *              Each info is related with only one title.
+     *              i.e: for foods -->
+     *              vegeteble: day order just
+     *              meal: foods with expiry date
+     *              .
+     *              .
+     *              .
+     */
+    public void addProperty(String title, String info) {
+        properties.put(title, info);
+    }
+
+    public void removeProperty(String title) {
+        properties.remove(title);
+    }
+
+    public HashMap<String, String> getProperties() {
+        return properties;
+    }
+
+    public boolean doesProductExist(Product product) {
+        return products.contains(product);
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public Product getProduct(String name) {
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            Product product = iterator.next();
+            if (product.getName().equalsIgnoreCase(name)) {
+                return product;
+            }
+        }
+        return null;
     }
 }
