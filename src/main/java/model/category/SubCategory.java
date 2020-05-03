913207a6403fd01606ad725905b5d0bec7400cd1
mahdi hadi <58135026+mahdihadiam@users.noreply.github.com>
2020-05-03 12:02:43 +0430

    Overall look at sub-category of Category abstract

--- src/main/java/model/category/SubCategory.java
+++ src/main/java/model/category/SubCategory.java
@@ -1,22 +1,48 @@
 package model.category;
 
+import model.others.Product;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class SubCategory extends Category {
-    private Category mainCategory;
+    private static ArrayList<SubCategory> subCategories;
 
-
-    public SubCategory() {
+    public SubCategory(String name) {
+        super(name);
     }
 
-    @Override
-    public String categoryInfoForSending() {
+    public static boolean doesSubCategoryExist(String name) {
+        Iterator<SubCategory> iterator = SubCategory.subCategories.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getName().equalsIgnoreCase(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void addSubCategory(SubCategory subCategory) {
+        SubCategory.subCategories.add(subCategory);
+    }
+
+    public static void removeSubCategory(String name) {
+        Iterator<SubCategory> iterator = SubCategory.subCategories.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getName().equalsIgnoreCase(name)) {
+                iterator.remove();
+                break;
+            }
+        }
+    }
+
+    public static SubCategory getSubCategory(String name) {
+        Iterator<SubCategory> iterator = SubCategory.subCategories.iterator();
+        while (iterator.hasNext()) {
+            SubCategory subCategory = iterator.next();
+            if (subCategory.getName().equalsIgnoreCase(name)) {
+                return subCategory;
+            }
+        }
         return null;
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "SubCategory{}";
     }
 }
