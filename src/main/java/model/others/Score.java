8a5e515ca1a46a87c90d94effae71b8edce3ecd5
mahdi hadi <58135026+mahdihadiam@users.noreply.github.com>
2020-05-03 12:18:29 +0430

    Update Score.java

--- src/main/java/model/others/Score.java
+++ src/main/java/model/others/Score.java
@@ -1,43 +1,37 @@
-package model.others;
+package model.other;
 
 import model.user.User;
 
 public class Score {
-    private User whoSubmitScore;
     private int score;
+    private User buyer;
     private User seller;
-    private Product product;
 
-
-    public Score() {
-    }
-
-    public void setWhoSubmitScore(User whoSubmitScore) {
-        this.whoSubmitScore = whoSubmitScore;
+    /**
+     * constructor may get only buyer and seller
+     * because it wouldn't happen to change these two
+     * but score maybe changes
+     * 
+     * @param buyer     person who rates
+     * @param seller    one kind of product may has many vendors
+     *                  so we differ sellers from each other.
+     *                  each seller has a unique rating for a product type
+     */
+    public Score(User buyer, User seller) {
+        this.buyer = buyer;
+        this.seller = seller;
     }
 
     public void setScore(int score) {
         this.score = score;
     }
 
-    public void setSeller(User seller) {
-        this.seller = seller;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public User getWhoSubmitScore() {
-        return whoSubmitScore;
-    }
-
     public int getScore() {
         return score;
     }
 
-    public Product getProduct() {
-        return product;
+    public User getBuyer() {
+        return buyer;
     }
 
     public User getSeller() {
