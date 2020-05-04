8f4b41358d48bd6189b132c4681efea7f24d22f0
alipour79 <56475612+alipour79@users.noreply.github.com>
2020-05-05 00:54:40 +0430

    Revert "Revert "Adding classes which use to sending message from controller to view and view to controller""
    This reverts commit d73577ff133a7ec18ccccf52360732a44753e0ae.

---
+++ src/main/java/model/send/receive/UserInfo.java
@@ -0,0 +1,79 @@
+package model.send.receive;
+
+public class UserInfo extends ServerMessage {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private double money;
+    private String companyName;
+    private String companyInfo;
+
+    public UserInfo() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyInfo() {
+        return companyInfo;
+    }
+
+    public void setCompanyInfo(String companyInfo) {
+        this.companyInfo = companyInfo;
+    }
+}
