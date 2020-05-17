d34a280af1800c3834c3d1f89884a0f624828d27
alipour79 <56475612+alipour79@users.noreply.github.com>
2020-05-17 19:33:51 +0430

    update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+import database.Database;
 import view.menu.UserMenu.customer.CustomerPanelMenu;
 
 public class Main {
     public static void main(String[] args) {
+        new Database().startDatabaseLoading();
         new CustomerPanelMenu(null).autoExecute();
-
     }
 }
