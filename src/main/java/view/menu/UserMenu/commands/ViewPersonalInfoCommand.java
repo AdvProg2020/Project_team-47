ec2c15ea39bd84f38c6d6f1c4ca0d58ef5042fd8
10 <amirmohammadisazadeh@gmail.com>
2020-05-18 02:09:57 +0430

    editing view personal info command

--- src/main/java/view/menu/UserMenu/commands/ViewPersonalInfoCommand.java
+++ src/main/java/view/menu/UserMenu/commands/ViewPersonalInfoCommand.java
@@ -1,6 +1,7 @@
 package view.menu.UserMenu.commands;
 
 import model.send.receive.ServerMessage;
+import model.send.receive.UserInfo;
 import view.ViewAttributes;
 import view.ViewToController;
 import view.command.Command;
@@ -20,6 +21,7 @@ public class ViewPersonalInfoCommand extends Command {
         sendMessageToViewToController();
         ServerMessage serverMessage = ViewToController.getServerMessage();
         if (serverMessage.getType().equals("Successful")) {
+            showUserPersonalInfo(serverMessage);
             this.getMenu().findSubMenuWithName("personal info menu").autoExecute();
         } else {
             System.out.println(serverMessage.getFirstString());
@@ -27,22 +29,34 @@ public class ViewPersonalInfoCommand extends Command {
 
     }
 
+    private void showUserPersonalInfo(ServerMessage serverMessage) {
+        UserInfo userInfo = serverMessage.getUserInfo();
+
+        System.out.println("username : " + userInfo.getUsername());
+        System.out.println("first name : " + userInfo.getFirstName());
+        System.out.println("last name : " + userInfo.getLastName());
+        System.out.println("email : " + userInfo.getEmail());
+        System.out.println("phone number : " + userInfo.getPhoneNumber());
+        /*if (!userInfo.getType().equals("manager")) {
+            System.out.println(": " + userInfo.getMoney());
+        }*/
+        /*System.out.println(" : " + userInfo.);
+        System.out.println(" : " + userInfo.);
+        System.out.println(" : " + userInfo.);*/
+    }
+
     private void sendMessageToViewToController() {
         ViewToController.setViewMessage("view personal info");
         ViewToController.sendMessageToController();
     }
 
-//    private void checkIfUserWantToSignIn() {
-//        OutputQuestions.goToSignInMenu();
-//        String answer = Menu.getInputCommandWithTrim();
-//        if (answer.equalsIgnoreCase("no")) {
-//
-//        } else if (answer.equalsIgnoreCase("yes")) {
-//            // todo
-//        } else {
-//            OutputErrors.invalidInputCommand();
-//            checkIfUserWantToSignIn();
-//        }
-//    }
-
 }
+
+/*username;
+private String firstName;
+private String lastName;
+private String email;
+private String phoneNumber;
+private double money;
+private String companyName;
+private String companyInfo;*/
