bb53dc95ffeca0eb543f8ffaabca2d9cf5cd4457
alipour79 <56475612+alipour79@users.noreply.github.com>
2020-05-19 15:30:06 +0430

    remove UserPanel controller

--- src/test/java/controller/ProductControllerTest.java
+++ src/test/java/controller/ProductControllerTest.java
@@ -3,7 +3,6 @@ package controller;
 import org.junit.Test;
 
 public class ProductControllerTest {
-    private ProductController productController = new ProductController();
 
     @Test
     public void showProduct() {
