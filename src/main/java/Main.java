e03b52ab22294dbd839012c8467f87ff5963b278
alipour79 <56475612+alipour79@users.noreply.github.com>
2020-05-19 18:10:25 +0430

    Clean up main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 import database.Database;
 import view.menu.UserMenu.customer.CustomerPanelMenu;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
         new Database().startDatabaseLoading();
