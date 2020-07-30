1129f69d03f9cca9163e95857c0535141e380b57
mahdi hadi <58135026+mahdihadiam@users.noreply.github.com>
2020-07-30 04:45:55 -0700

    Add files via upload

---
+++ src/main/java/graphic/panel/customer/AuctionsPage.java
@@ -0,0 +1,41 @@
+package graphic.panel.customer;
+
+import graphic.PageController;
+import javafx.scene.Scene;
+import model.send.receive.ClientMessage;
+import model.send.receive.ServerMessage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AuctionsPage extends PageController {
+
+    private static PageController controller;
+
+    public static PageController getInstance() {
+        return controller;
+    }
+
+    public static Scene getScene() {
+        return getScene("/fxml/panel/seller/auction/AuctionsPage.fxml");
+    }
+
+    @Override
+    public void clearPage() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void fresh() {
+        ServerMessage answer = send(new ClientMessage("give auctions"));
+    }
+}
